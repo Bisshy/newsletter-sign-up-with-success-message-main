1027c66b99edcf3363cbbc801c22b2d4bcb4d5f2
Bishop <oakinronbi@gmail.com>
2024-08-30 13:38:17 +0100

    final 2
    javascript updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const container = document.getElementById('container')
-const form = document.getElementById('form')
+const formContainer = document.getElementById('form')
 const email = document.getElementById('email')
 const emailCheck =  /^([a-zA-Z0-9\._]+)@([a-zA-z0-9])+.([a-z]+)(.[a-z]+)?$/
 const emailError = document.getElementById('email-error')
@@ -7,15 +7,19 @@ const submitButton = document.getElementById('submit__button')
 const dimissButton = document.getElementById('dismiss-button')
 const successMessage = document.getElementById('success-message')
 const inputActive = document.querySelector('.active')
-form.addEventListener('submit', function(e){
+const submittedEmail = document.getElementById('submitted-email')
+
+formContainer.addEventListener('submit', function(e){
     e.preventDefault()
     const data = Object.fromEntries(new FormData(e.target))
-})
+    console.log(data)
 
+})
 submitButton.addEventListener('click', function(){
     if (email.value.match(emailCheck)){
         container.classList.add('hide')
         successMessage.classList.remove('hide')
+        submittedEmail.textContent = email.value
     } else{
          emailError.innerHTML = "Valid Email Required"
          inputActive.classList.add('active')
